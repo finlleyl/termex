7f88950c01ce75144dce7b3c09b9b0d55d5dccde
finlleyl <ilya_chibugaev@mail.ru>
2024-12-20 00:29:38 +0300

    fix

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,75 +1,79 @@
-import math
-import numpy as np
+import numpy as n
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-R = 5.0  # радиус большой шестерни
-r = 1.5  # радиус маленькой шестерни
-omega = 0.5  # угловая скорость
+#задается интервал
+step = 500
+t = n.linspace(0, 10, step)
+#задается функция
+phi = n.sin(2*t)
+theta = n.linspace(0, -2*n.pi, step)
 
-iterations = 200
-T = np.linspace(0, 10, iterations)
+O = 0
+r2 = 0.5#внешний радиус пружины
+r1 = 0.2#внутренний радиус пружины
 
+R1 = 3 #радиус кольца
 
-def theta(t):
-    return omega * t
+#координаты точки А
+Xa = (R1 - 1) * n.cos(n.pi/2+phi)
+Ya = (R1 - 1) * n.sin(n.pi/2+phi)
+#координаты точки Б
+Xb = - R1 * n.cos(n.pi/2+theta)
+Yb = R1 * n.sin(n.pi/2+theta)
 
+#окно и грaфик
+fgr = plt.figure()
+grf = fgr.add_subplot(1, 1, 1)
+grf.axis('equal')
+grf.set(xlim = [-5, 5], ylim = [-5, 5])
+grf.set_aspect( 1 )
 
-def phi(t):
-    return 2.0 * omega * t
+#треугольник
+grf.plot([0,0.5,-0.5,0], [0,-1,-1,0], color = 'black')
 
+#точки на графике
+p1 = grf.plot(O, O, marker = 'o', color = 'black')[0]
+pA = grf.plot(Xa[0], Ya[0], marker = 'o', color = 'black')[0]
+pB = grf.plot(Xb[0], Yb[0], marker = 'o', color = 'black')[0]
 
-# Позиции точки A:
-# A движется по окружности радиуса (R - r) вокруг O
-# X_A(t) = (R - r)*cos(phi(t))
-# Y_A(t) = (R - r)*sin(phi(t))
+#прямая ОА
+O1 = grf.plot([Xa[0], O],[Ya[0], O],color = 'black')[0]
 
-XA = (R - r) * np.cos(phi(T))
-YA = (R - r) * np.sin(phi(T))
-
-
-def draw_circle(center_x, center_y, radius=1.0, resolution=100):
-    angles = np.linspace(0, 2 * math.pi, resolution)
-    x = center_x + radius * np.cos(angles)
-    y = center_y + radius * np.sin(angles)
-    return x, y
-
-
-fig, ax = plt.subplots()
-ax.set_aspect('equal', adjustable='box')
-ax.set_xlim(-R - 2, R + 2)
-ax.set_ylim(-R - 2, R + 2)
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_title('Анимация системы шестерен')
-
-# (Шестерня 1)
-gear1_x, gear1_y = draw_circle(0, 0, radius=R)
-gear1_line, = ax.plot(gear1_x, gear1_y, 'b')
-
-# (Шестерня 2)
-gear2_x, gear2_y = draw_circle(XA[0], YA[0], radius=r)
-gear2_line, = ax.plot(gear2_x, gear2_y, 'r')
-
-# кривошип OA
-crank_line, = ax.plot([0, XA[0]], [0, YA[0]], 'k')
-
-point_O, = ax.plot(0, 0, 'ko')
-point_A, = ax.plot(XA[0], YA[0], 'ro')
-
-
-def update(i):
-    gear2_x, gear2_y = draw_circle(XA[i], YA[i], radius=r)
-    gear2_line.set_data(gear2_x, gear2_y)
-
-    crank_line.set_data([0, XA[i]], [0, YA[i]])
-
-    point_A.set_data([XA[i]], [YA[i]])
-
-    return gear2_line, crank_line, point_A
+#большая окружность  
+circle = plt.Circle(( O, O ), R1 , fill = False)
+grf.add_artist(circle)
+               
+#маленькая окружность
+circleA = plt.Circle(( Xa[0], Ya[0]), 1 , fill = False)
+grf.add_artist(circleA)
 
 
 
-anim = FuncAnimation(fig, update, frames=iterations, interval=50, blit=True)
+#спиральная пружина
+Ns = 2
+numpnts = n.linspace(0, 1, 50*Ns+1)
+Betas = numpnts*(Ns * 2*n.pi - phi[0])
+Xs = ((r2-r1)*numpnts)*n.cos(Betas + n.pi/2)
+Ys = ((r2-r1)*numpnts)*n.sin(Betas + n.pi/2)
 
-plt.show()
+SpPruzh = grf.plot(Xs, Ys, color = 'black')[0]
+
+
+#анимация
+def run(i):
+    pA.set_data([Xa[i]], [Ya[i]])
+    pB.set_data([Xb[i]], [Yb[i]])
+    O1.set_data([Xa[i], O], [Ya[i], O])
+    circleA.center = (Xa[i],Ya[i])
+
+    Betas = numpnts*(Ns * 2*n.pi - phi[i])
+    Xs = -((r2-r1)*numpnts)*n.cos(Betas+n.pi/2)
+    Ys = ((r2-r1)*numpnts)*n.sin(Betas+n.pi/2)
+
+    SpPruzh.set_data(Xs, Ys)
+    return
+
+anim = FuncAnimation(fgr, run, interval = 1, frames = step)
+
+fgr.show()
